9a2d0ecb2a7af27f0244626f53f7ef7508b5da64
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-10-05 13:17:44 +0530

    Refactor ShowMore component to use setLimit prop instead of router.push

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -4,13 +4,10 @@ import { useRouter } from "next/navigation";
 import CustomButton from "./CustomButton";
 import { updateSearchParams } from "@/utils";
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-  const router = useRouter();
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
-    const newPathName = updateSearchParams("limit", `${newLimit}`);
-
-    router.push(newPathName);
+    setLimit(newLimit);
   };
 
   return (
