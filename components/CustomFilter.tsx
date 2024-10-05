885b39a1dc2668825b372b81658e433ed70de73e
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-10-05 13:17:30 +0530

    Refactor CustomFilter component to use setFilter prop instead of router.push

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -7,21 +7,16 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const router = useRouter();
+const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
-  const handleUpdateParams = (e: { title: string; value: string }) => {
-    const newPathName = updateSearchParams(title, e.value.toLowerCase());
 
-    router.push(newPathName);
-  };
   return (
     <div className="w-fit">
       <Listbox
         value={selected}
         onChange={(e) => {
           setSelected(e);
-          handleUpdateParams(e);
+          setFilter(e.value);
         }}
       >
         <div className="relative w-fit z-10">
