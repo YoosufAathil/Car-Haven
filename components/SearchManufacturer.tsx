4153ed5694ce993dc76d8a4cc3c571474b784503
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-10-05 13:17:39 +0530

    Refactor SearchManufacturer component to use selected prop instead of manufacturer and setManufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,8 +6,8 @@ import { manufacturers } from "@/constants";
 import { useState, Fragment } from "react";
 
 const SearchManufacturer = ({
-  manufacturer,
-  setManufacturer,
+  selected,
+  setSelected,
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
 
@@ -22,7 +22,7 @@ const SearchManufacturer = ({
         );
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={selected} onChange={setSelected}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
